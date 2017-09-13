ce74e78d25327b4656a9910f42cf8daa8673f1cd
Daniel <daniel@filmstro.com>
2017-09-13 21:16:04 +0100

    Initial commit of buffer classes

---
+++ ff_buffers.h
@@ -0,0 +1,55 @@
+/*
+ ==============================================================================
+ Copyright (c) 2017 Filmstro Ltd. / Foleys Finest Audio UG - Daniel Walz
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without modification,
+ are permitted provided that the following conditions are met:
+ 1. Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+ 3. Neither the name of the copyright holder nor the names of its contributors
+    may be used to endorse or promote products derived from this software without
+    specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ OF THE POSSIBILITY OF SUCH DAMAGE.
+ ==============================================================================
+
+ BEGIN_JUCE_MODULE_DECLARATION
+
+ ID:            ff_buffers
+ vendor:        Foleys Finest Audio UG / Filmstro Ltd.
+ version:       0.9.0
+ name:          Meters with GUI and LookAndFeel
+ description:   Contains a metering Component, that can display live peak and RMS values 
+ dependencies:  juce_core,juce_audio_basics
+ website:       http://www.foleysfinest.com/
+ license:       BSD V2 3-clause
+
+ END_JUCE_MODULE_DECLARATION
+
+ ==============================================================================
+ */
+
+#pragma once
+
+#include <juce_core/juce_core.h>
+#include <juce_audio_basics/juce_audio_basics.h>
+
+
+#include "ff_buffers_AudioBufferFIFO.h"
+#include "ff_buffers_CircularLagrangeInterpolator.h"
+#include "ff_buffers_ElasticDelayBuffer.h"
+
+
---
+++ ff_buffers_ElasticDelayBuffer.h
@@ -0,0 +1,145 @@
+/*
+  ==============================================================================
+
+    ElasticDelayBuffer.h
+    Created: 30 Jul 2017 12:56:50am
+    Author:  Dobby
+
+  ==============================================================================
+*/
+
+#pragma once
+
+template <typename SampleType>
+class ElasticDelayBuffer {
+    juce::AudioBuffer<SampleType>           buffer;
+
+    juce::OwnedArray<CircularLagrangeInterpolator> resamplers;
+
+    int writePosition = 0;
+    int readPosition  = 0;
+    
+    std::atomic<int> numDelaySamples;
+    
+    double maxResamplingFactor = 8.0;
+
+public:
+    ElasticDelayBuffer ()
+    {
+    }
+    
+    void setSize (const int numChannels, const int numSamples, const double sampleRate)
+    {
+        buffer.setSize (numChannels, numSamples, false, true);
+        while (resamplers.size() < numChannels) {
+            resamplers.add (new CircularLagrangeInterpolator());
+        }
+        while (resamplers.size() > numChannels) {
+            resamplers.removeLast();
+        }
+        if (writePosition >= numSamples) {
+            writePosition = 0;
+            buffer.clear();
+            for (auto r : resamplers) {
+                r->reset();
+            }
+        }
+        updateActualSampleDelay();
+    }
+
+    void setNumSamplesDelay (const int delay) {
+        jassert (delay < buffer.getNumSamples());
+        const int assumedRead = writePosition - delay;
+        readPosition = (assumedRead < 0) ? assumedRead + buffer.getNumSamples() : assumedRead;
+
+        updateActualSampleDelay();
+    }
+
+    void setMaxResamplingFactor (const double factor) {
+        maxResamplingFactor = factor;
+    }
+
+    int getActualSampleDelay () const
+    {
+        return numDelaySamples;
+    }
+
+    void updateActualSampleDelay ()
+    {
+        numDelaySamples = (writePosition < readPosition) ? writePosition + buffer.getNumSamples() - readPosition : writePosition - readPosition;
+    }
+
+    void pushBlock (const juce::AudioBuffer<SampleType>& inputBuffer, const int numSamples, const SampleType gain = 1.0f)
+    {
+        jassert (buffer.getNumSamples()  >  numSamples);
+        jassert (buffer.getNumChannels() == inputBuffer.getNumChannels());
+
+        const int available = buffer.getNumSamples() - (writePosition + numSamples);
+        if (available >= 0) {
+            for (int i=0; i < buffer.getNumChannels(); ++i)
+                buffer.copyFrom (i, writePosition, inputBuffer.getReadPointer (i), numSamples, gain);
+
+            writePosition += numSamples;
+        }
+        else {
+            for (int i=0; i < buffer.getNumChannels(); ++i) {
+                buffer.copyFrom (i, writePosition, inputBuffer.getReadPointer (i), numSamples + available, gain);
+                buffer.copyFrom (i, 0, inputBuffer.getReadPointer (i, numSamples + available), -available, gain);
+            }
+            writePosition = -available;
+        }
+
+        updateActualSampleDelay();
+    }
+
+    void addToPushedBlock (const juce::AudioBuffer<SampleType>& inputBuffer, const int numSamples, const SampleType gain = 1.0f)
+    {
+        jassert (buffer.getNumSamples()  >  numSamples);
+        jassert (buffer.getNumChannels() == inputBuffer.getNumChannels());
+
+        int previousWrite = writePosition - numSamples;
+        if (previousWrite < 0)
+            previousWrite += buffer.getNumSamples();
+        const int available = buffer.getNumSamples() - (previousWrite + numSamples);
+        if (available >= 0) {
+            for (int i=0; i < buffer.getNumChannels(); ++i)
+                buffer.addFrom (i, previousWrite, inputBuffer.getReadPointer (i), numSamples, gain);
+        }
+        else {
+            for (int i=0; i < buffer.getNumChannels(); ++i) {
+                buffer.addFrom (i, previousWrite, inputBuffer.getReadPointer (i), numSamples + available, gain);
+                buffer.addFrom (i, 0, inputBuffer.getReadPointer (i, numSamples + available), -available, gain);
+            }
+        }
+    }
+
+    void pullBlock (juce::AudioBuffer<SampleType>& outputBuffer, const int numSamples, const int delayTime)
+    {
+        jassert (buffer.getNumSamples()  >  outputBuffer.getNumSamples());
+        jassert (buffer.getNumChannels() == outputBuffer.getNumChannels());
+        jassert (buffer.getNumChannels() == resamplers.size());
+
+        int currentDelay = writePosition - readPosition;
+        if (currentDelay < 0) currentDelay += buffer.getNumSamples();
+
+        double difference = (currentDelay - numSamples) - delayTime;
+        double factor = 1.0 + (difference / (numSamples * 8.0));
+        if (factor < 0.0001)
+            factor = 0.0001;
+        if (factor > maxResamplingFactor)
+            factor = maxResamplingFactor;
+
+        int used = 0;
+        for (int channel=0; channel < outputBuffer.getNumChannels(); ++channel) {
+            used = resamplers.getUnchecked (channel)->process (factor, buffer.getReadPointer (channel, readPosition),
+                                                               outputBuffer.getWritePointer (channel),
+                                                               numSamples,
+                                                               buffer.getNumSamples() - readPosition,
+                                                               buffer.getNumSamples());
+        }
+        readPosition = (readPosition + used) % buffer.getNumSamples();
+        
+        updateActualSampleDelay();
+    }
+};
+
